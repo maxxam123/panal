829717429d9602811e2dba267e8b2cda2a28c961
max <hennighausenmax@gmail.com>
2022-05-04 13:16:22 +0200

    added chart & featuredInfo

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
-// import { Chart } from '../../components/chart/Chart';
-// import { FeaturedInfo } from '../../components/featuredInfo/FeaturedInfo';
+import { Chart } from '../../components/chart/Chart';
+import { FeaturedInfo } from '../../components/featuredInfo/FeaturedInfo';
 import './home.css';
 // import { WidgetLg } from '../../components/widgetLg/WidgetLg';
 import { useEffect, useMemo, useState } from 'react';
@@ -54,16 +54,15 @@ export default function Home() {
 
   return (
     <div className="home">
-      {/* <FeaturedInfo /> */}
-      {/* <Chart
+      <FeaturedInfo />
+      <Chart
         data={userStats}
         // data={userData}
         title="User Analytics"
         grid
         dataKey="Active User"
-      /> */}
+      />
       <div className="homeWidget">
-        home
         {/* <WidgetSm /> */}
         {/* <WidgetLg /> */}
       </div>
